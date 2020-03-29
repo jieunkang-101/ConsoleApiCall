4952082a6f2a3c28f5feaee8f6a8cc1f0b0e4942
Jieun Kang <jieunkang101@gmail.com>
2020-03-29 12:49:52 -0700

    APIs with RestSharp

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Threading.Tasks;
+using RestSharp;
+
+namespace ApiTest
+{
+  class Program
+  {
+    static void Main()
+    {
+      var apiCallTask = ApiHelper.ApiCall("KHvQIcHEqKnocBMrlRLAiBY63Msr8Yeg");
+      var result = apiCallTask.Result;
+      
+      JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(result);
+      Console.WriteLine(jsonResponse["results"]);
+    }
+  }
+
+  class ApiHelper
+  {
+    public static async Task<string> ApiCall(string apiKey)
+    {
+      RestClient client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+      RestRequest request = new RestRequest($"home.json?api-key={apiKey}", Method.GET);
+      var response = await client.ExecuteTaskAsync(request);
+      return response.Content;
+    }
+  }
+}
